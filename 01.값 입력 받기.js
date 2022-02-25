ea89631a63a70aca215f086f875c0d2e6ba99b10
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-02-25 21:12:47 +0900

    Add files via upload

---
+++ 01.값 입력 받기.js
@@ -0,0 +1,53 @@
+/*
+Receiving a value
+
+To receive a value through the console in JavaScript, you can use the readline module.
+To import the readline module, use the following code:
+
+
+    const readline = require("readline");
+
+
+After import the readline module, you can use the readline.createInterface() method to create a readline interface.
+This interface object is used to create an input stream and output stream for the readline module.
+You can use the following code to create a readline interface:
+
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+
+The rl object can be used as follows:
+
+
+    rl.on("line", (line) => {
+        // This code runs after the line-by-line input.
+        // The inputted values will be stored to the line object.
+        rl.close(); // Without close(), the input will be kept.
+    });
+    rl.on('close', () => {
+        //You can put the code here that will run after the input is done.
+    })
+
+*/
+
+
+//The code below will show the inputted value to the console without any change.
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    console.log(line);
+    rl.close();
+});
+
+rl.on("close", () => {
+    process.exit();
+});
